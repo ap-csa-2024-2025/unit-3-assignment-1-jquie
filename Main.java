30994efb651cfb71cb46da1d21473a7f9afeaec0
jquie <jacquelynphan03@gmail.com>
2024-11-19 18:10:03 +0000

    done!

--- Main.java
+++ Main.java
@@ -4,6 +4,17 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    System.out.println("Give me two numbers");
+    Scanner userInput = new Scanner(System.in);
+    int x = userInput.nextInt();
+    int y = userInput.nextInt();
+    Rectangle r1 = new Rectangle (x, y);
+    Rectangle r2 = new Rectangle (y, x);
+    boolean isRotated = (r1.getLength() == r2.getWidth()) && (r1.getWidth() == r2.getLength()); 
+    boolean isCongruent = r1.equals(r2) || isRotated;
+    boolean isSimilar = isCongruent || (r1.getLength()/r2.getLength()) == (r1.getWidth()/r2.getWidth());
+    System.out.println(isRotated);
+    System.out.println(isCongruent);
+    System.out.println(isSimilar);
   }
 }
